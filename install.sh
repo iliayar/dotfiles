452bb8a98068008a5388396243d06b6a23fde1a6
Ilya <iliayar3@gmail.com>
2019-05-07 18:48:15 +0300

    Beautify

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-cp ./home/* ~/ -r
+echo 'Copying dofiles'
+cp home/* ~/ -r
+echo 'Install nebundle.vim:'
+mkdir ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
