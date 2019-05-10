1d0425da2f9a18de012739c77bab9022eb6a6a19
Ilya <iliayar3@gmail.com>
2019-05-10 16:24:36 +0300

    Zsh plugins added

--- install.sh
+++ install.sh
@@ -15,14 +15,19 @@ cp .config/i3blocks $HOME/.config/ -r
 cp .config/termite $HOME/.config/ -r
 cp .config/compton.conf $HOME/.config
 
-echo "Installing zsh, bash, vim configs"
+echo "Installing bash, vim configs"
 
 cp .Xresources $HOME/
 cp .vimrc $HOME/
 cp .bashrc $HOME/
 cp .zshrc $HOME/
+
+echo "Installing zsh"
+
 [ -d $HOME/.oh-my-zsh ] && rm -Rf $HOME/.oh-my-zsh
 git clone -q https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+git clone -q https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone -q  https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 [ ! -d $HOME/.vim ] && mkdir $HOME/.vim
 [ ! -d $HOME/.vim/bundle ] && mkdir $HOME/.vim/bundle
