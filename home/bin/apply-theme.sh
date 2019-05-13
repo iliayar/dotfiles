8bbf1065a7b8f5d33bb56444df7c09112fba1d97
iliayar <iliayar@mail.ru>
2019-05-13 13:21:29 +0300

    Savin backgrounds

--- home/bin/apply-theme.sh
+++ home/bin/apply-theme.sh
@@ -1,5 +1,5 @@
 ~/bin/color-utils -p ~/.Xresources -p $HOME/Themes/$1.x l mt ~/.config/termite/config mx ~/.resources/.Xresources mr ~/.Xresources
-nitrogen --head=1 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
-nitrogen --head=0 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
+nitrogen --save --head=1 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
+nitrogen --save --head=0 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
 xrdb -merge $HOME/.Xresources
 i3-msg restart
