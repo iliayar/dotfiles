2e140ef8f0df8972e51a4126618ba09eb351b3c3
Ilya <iliayar3@gmail.com>
2019-05-15 20:11:51 +0300

    Apply-theme script fixed

--- home/bin/apply-theme.sh
+++ home/bin/apply-theme.sh
@@ -1,4 +1,4 @@
-~/bin/color-utils -p ~/.Xresources -p $HOME/Themes/$1.x l mt ~/.config/termite/config mx ~/.resources/.Xresources mr ~/.Xresources
+~/bin/color-utils -p ~/.Xresources -p $HOME/Themes/$1.x l mt ~/.config/termite/config mx ~/.Xresources mr ~/.Xresources
 nitrogen --head=1 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
 nitrogen --head=0 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
 xrdb -merge $HOME/.Xresources
