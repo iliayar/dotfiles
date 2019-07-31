a07da625d147ef377d1d3b270a60b853168d741e
iliayar <iliayar3@gmail.com>
2019-07-31 21:12:33 +0300

    Plasma breeze replaced

--- home/bin/apply-theme.sh
+++ home/bin/apply-theme.sh
@@ -1,5 +1,7 @@
 ~/bin/color-utils -alpha $2 -p ~/.Xresources -p $HOME/Themes/$1.x l mt ~/.config/termite/config mx ~/.Xresources mr ~/.Xresources
+sudo cp $HOME/Themes/$1.png /usr/share/sddm/themes/sddm-sugar-candy/Backgrounds/Mountain.jpg
 nitrogen --head=1 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
 nitrogen --head=0 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
 xrdb $HOME/.Xresources
 i3-msg restart
+[ -e $HOME/Themes/$1.sh ] && $HOME/Themes/$1.sh
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd $DIR
 
 echo "Installing deps"
 
-sudo pacman -S -q --needed zsh termite rofi compton dunst udiskie sbxkb nitrogen awesome-terminal-fonts scrot pulsemixer imagemagick i3-scrot zathura ranger plasma
+sudo pacman -S -q --needed zsh termite rofi compton dunst udiskie sbxkb nitrogen awesome-terminal-fonts scrot pulsemixer imagemagick i3-scrot zathura rangera qt5-graphicaleffects qt5-quickcontrols2 qt5-svg
 
 if [[ ! -e /bin/yay ]]; then 
 cd /tmp
@@ -68,17 +68,18 @@ echo "Installing Themes"
 [ -d $HOME/Themes ] && rm -Rf $HOME/Themes
 git clone -q https://github.com/iliayar/MyThemes.git $HOME/Themes
 
-$HOME/bin/apply-theme.sh Monokai
-
 echo "Installing others"
 
 cd ../other
 
 sudo cp xorg.conf /etc/X11/xorg.conf
 
-sudo mkdir /usr/share/wallpapers/Custom
-sudo cp /home/iliayar/Themes/Monokai.png /usr/share/wallpapers/Custom/1920x1080.jpg
-sudo cp theme.conf.user /usr/share/sddm/themes/breeze/theme.conf.user
+git clone https://git.iliayar.ru/iliayar/sddm-sugar-candy
+sudo mv sddm-sugar-candy /usr/share/sddm/themes/
+
+sudo cp theme.conf.user /usr/share/sddm/themes/sddm-sugar-candy/theme.conf.user
 sudo cp sddm.conf /etc/sddm.conf
+
+$HOME/bin/apply-theme.sh Monokai
 
 cd $ST_PWD
