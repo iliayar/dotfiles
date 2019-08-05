f38534ff9e49315f869d14ff7db0276e8fea11dc
iliayar <iliayar3@gmail.com>
2019-08-05 19:13:38 +0300

    PATH var edited

--- home/bin/apply-theme.sh
+++ home/bin/apply-theme.sh
@@ -1,4 +1,4 @@
-~/bin/color-utils -alpha $2 -p ~/.Xresources -p $HOME/Themes/$1.x l mt ~/.config/termite/config mx ~/.Xresources mr ~/.Xresources
+$HOME/bin/color-utils -alpha $2 -p ~/.Xresources -p $HOME/Themes/$1.x l mt ~/.config/termite/config mx ~/.Xresources mr ~/.Xresources
 sudo cp $HOME/Themes/$1.png /usr/share/sddm/themes/sddm-sugar-candy/Backgrounds/Mountain.jpg
 nitrogen --head=1 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
 nitrogen --head=0 --set-zoom-fill $HOME/Themes/$1.png > /dev/null 2>&1
