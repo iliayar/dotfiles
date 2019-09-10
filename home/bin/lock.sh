820466b71dcab5c822659f6f34ec78b4f56b5c37
iliayar <iliayar3@gmail.com>
2019-09-10 13:11:10 +0300

    Fn buttons, nm-applet removed

--- home/bin/lock.sh
+++ home/bin/lock.sh
@@ -4,7 +4,10 @@ tmpbg='/tmp/screen.png'
 
 (( $# )) && { icon=$1; }
 
+
+[[ -e $tmpbg ]] && rm $tmpbg
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 #convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -i "$tmpbg"
+xset dpms force off
