9f2981e4e4cd705bbe0477762ab9bd55909e22aa
iliayar <iliayar3@gmail.com>
2020-07-25 09:44:37 +0300

    install fix

--- install.sh
+++ install.sh
@@ -350,12 +350,10 @@ xmonad() {
 }
 
 install_xmonad() {
-    cd ~/.xmonad
+    cd "~/.xmonad" || exit
 
     curl -sSL https://get.haskellstack.org/ | sh
     stack setup
-
-    cd ~/.xmonad
 
     git clone "https://github.com/xmonad/xmonad" xmonad-git
     git clone "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
