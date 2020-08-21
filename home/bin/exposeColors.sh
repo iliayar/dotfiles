7fb4539356ac514ee84b17e2b3d69b0bd5224b73
Ilya <iliayar3@gmail.com>
2020-08-21 22:56:25 +0300

    pywal again

---
+++ home/bin/exposeColors.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+cp ~/.cache/wal/colors.Xresources ~/.Xresources
+cp ~/.cache/wal/termite.conf ~/.config/termite/config
+cp ~/.cache/wal/Theme.hs ~/.xmonad/Theme.hs
+cp ~/.cache/wal/Theme.hs ~/.config/xmobar/Theme.hs
