7ccb645d4436517883176b0bdbe28e1e7cd132b6
Ilya <iliayar3@gmail.com>
2021-07-30 18:07:13 +0300

    Parsing agenda entries

--- home/bin/rust-blocks/src/agenda.rs
+++ home/bin/rust-blocks/src/agenda.rs
@@ -3,7 +3,7 @@ use super::*;
 use tokio::process::Command;
 use csv::Reader;
 use serde::{Deserialize, Deserializer};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, NaiveDate, NaiveTime, TimeZone};
 
 pub struct AgendaBlock {
     // fifo: Option<Mutex<File>>
@@ -18,63 +18,83 @@ impl AgendaBlock {
 }
 
 #[derive(Debug,Deserialize)]
-enum State {
+enum AgendaTodo {
     TODO,
     FIXME,
     DONE,
+    #[serde(rename = "")]
+    NONE,
+}
+
+#[derive(Debug,Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum EventType {
+    Scheduled,
+    Deadline,
+    Block,
+    #[serde(rename="past-scheduled")]
+    PastScheduled,
+    #[serde(rename="upcoming-deadline")]
+    UpcomingDeadline,
+}
+
+#[derive(Debug)]
+enum AgendaTime {
+    Exact(DateTime<Utc>),
+    Interval(DateTime<Utc>, DateTime<Utc>),
+    Day(DateTime<Utc>),
+    None,
 }
 
 #[derive(Debug)]
 struct AgendaRecord {
-    file: String,
-    headline: String,
-    scheduled: String,
-    state: State,
+    category: String,
+    head: String,
+    event_type: EventType,
+    todo: AgendaTodo,
     // unknown1: String,
-    date: DateTime<Utc>,
-    // unknown2: String,
+    date: AgendaTime,
+    // unknown2: String, // Time(combined in date field)
     // unknown3: String,
     // unknown4: String,
-    // unknown5: String,
+    priority: u32,
     // unknown6: String,
 }
 
 #[derive(Debug, Deserialize)]
-struct AgendaRecordHelper(String, String, String, State, String,
-			  #[serde(with = "org_date_format")]
-			  DateTime<Utc>,
-			  String, String, String, String, String);
-
-mod org_date_format {
-    use chrono::{DateTime, Utc, TimeZone};
-    use serde::{self, Deserialize, Deserializer};
-
-    const FORMAT: &'static str = "%Y-%m-%d";
-
-    pub fn deserialize<'de, D>(
-        deserializer: D,
-    ) -> Result<DateTime<Utc>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-	// FIXME: Only date fails
-        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
-    }
-}
+struct AgendaRecordHelper(String, String, EventType, AgendaTodo, String, String, String, String, String, u32, String);
 
 impl<'de> Deserialize<'de> for AgendaRecord {
     fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
     where D: Deserializer<'de> {
 	Deserialize::deserialize(deserializer)
-	    .map(|a: AgendaRecordHelper|
-		 AgendaRecord {
-		     file: a.0,
-		     headline: a.1,
-		     scheduled: a.2,
-		     state: a.3,
-		     date: a.5,
-		 })
+	    .map(|a: AgendaRecordHelper| {
+		let date = if let Some(date) = NaiveDate::parse_from_str(&a.5, "%Y-%m-%d").ok() {
+		    let times: Vec<DateTime<Utc>> = a.6
+			.trim_end_matches(|c| c == '.')
+			.split("-")
+			.flat_map(|s| NaiveTime::parse_from_str(s, "%H:%M").into_iter())
+			.map(|time| Utc.from_utc_datetime(&date.and_time(time)))
+			.collect();
+		    match times.len() {
+			2 => AgendaTime::Interval(times[0], times[1]),
+			1 => AgendaTime::Exact(times[0]),
+			0 => AgendaTime::Day(Utc.from_utc_date(&date).and_hms(23, 59, 59)),
+			_ => AgendaTime::None
+		    }
+		} else {
+		    AgendaTime::None
+		};
+
+		AgendaRecord {
+		    category: a.0,
+		    head: a.1,
+		    event_type: a.2,
+		    todo: a.3,
+		    date,
+		    priority: a.9
+		}
+	    })
     }
 }
 
@@ -108,6 +128,9 @@ impl Block for AgendaBlock
 	    for result in rdr.deserialize::<AgendaRecord>() {
 		println!("{:?}", result);
 	    }
+	    // for result in rdr.records() {
+	    // 	println!("{:?}", result);
+	    // }
 	}
     }
 
