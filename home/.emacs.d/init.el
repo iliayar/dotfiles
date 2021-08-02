bb31a16b66cd400dd39a6d94e1bf5acf713d9a4f
Ilya <iliayar3@gmail.com>
2021-08-03 02:49:05 +0300

    Add org-roam + pretify org-mode

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,68 +1,20 @@
 (setq comp-deferred-compilation t)
 (org-babel-load-file "~/.emacs.d/config.org")
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#1d2021" "#ee4531" "#aeb124" "#edb32c" "#83a598" "#d3869b" "#8ec07c" "#d5c4a1"])
- '(company-box-show-single-candidate 'always)
- '(custom-safe-themes
-   '("5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "5d09b4ad5649fea40249dd937eaaa8f8a229db1cec9a1a0ef0de3ccf63523014" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "cf3d5d77679f7daed6a2c863e4f2e30427d5e375b254252127be9359957502ec" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "e5dc5b39fecbeeb027c13e8bfbf57a865be6e0ed703ac1ffa96476b62d1fae84" default))
  '(ein:output-area-inlined-images t)
- '(fci-rule-color "#665c54")
- '(flycheck-display-errors-function 'ignore)
- '(jdee-db-active-breakpoint-face-colors (cons "#151818" "#83a598"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#151818" "#aeb124"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#151818" "#282b2c"))
- '(jdee-server-dir "~/.local/bin/jars")
- '(lsp-eldoc-enable-hover t)
- '(lsp-haskell-completion-snippets-on nil)
- '(lsp-haskell-hlint-on t)
- '(objed-cursor-color "#ee4531")
  '(package-selected-packages
-   '(ox-rss nim-mode jupyter ein nlinum-relative elfeed-org elfeed-goodies elfeed lsp-ivy gcmh haskell-lsp evil-mu4e list-packages-ext mu4e smooth-scroll smooth-scrolling rustic rust-mode org-alert tramp-hdfs gnuplot atom-one-dark-theme lsp-haskell presentation highlight-doxygen lsp-java lsp-pyright cmake-mode cpputils-cmake cmake-ide org-pomodoro org-special-block-extras company-ctags ox-reveal org-super-agenda code-stats lsp-intellij dockerfile-mode docker ob-ipython graphviz-dot-mode highlight-indentation vterm latex-pretty-symbols pretty-symbols web-mode zzz-to-char dtrt-indent anaconda-mode company-anaconda unicode-fonts gradle-mode kotlin-mode origami ewal-doom-themes ewal impatient-mode company-lsp format-all yaml-mode ox-md hl-todo evil-multiedit treemacs-all-the-icons evil-collection use-package))
- '(pdf-view-midnight-colors (cons "#d5c4a1" "#1d2021"))
- '(resize-mini-windows nil)
- '(rustic-ansi-faces
-   ["#1d2021" "#ee4531" "#aeb124" "#edb32c" "#83a598" "#d3869b" "#8ec07c" "#d5c4a1"])
- '(send-mail-function 'mailclient-send-it)
- '(tetris-x-colors
-   [[229 192 123]
-    [97 175 239]
-    [209 154 102]
-    [224 108 117]
-    [152 195 121]
-    [198 120 221]
-    [86 182 194]])
- '(vc-annotate-background "#1d2021")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#aeb124")
-    (cons 40 "#c3b126")
-    (cons 60 "#d8b229")
-    (cons 80 "#edb32c")
-    (cons 100 "#f18f2e")
-    (cons 120 "#f66c31")
-    (cons 140 "#fb4934")
-    (cons 160 "#ed5d56")
-    (cons 180 "#e07178")
-    (cons 200 "#d3869b")
-    (cons 220 "#dc7077")
-    (cons 240 "#e55a54")
-    (cons 260 "#ee4531")
-    (cons 280 "#bc3e2f")
-    (cons 300 "#8b382e")
-    (cons 320 "#59312d")
-    (cons 340 "#665c54")
-    (cons 360 "#665c54")))
- '(vc-annotate-very-old-color nil)
- '(warning-suppress-types '((comp) (comp)))
- '(window-adjust-process-window-size-function 'ignore))
+    '(org-roam ox-rss nim-mode jupyter ein nlinum-relative elfeed-org elfeed-goodies elfeed lsp-ivy gcmh haskell-lsp evil-mu4e list-packages-ext mu4e smooth-scroll smooth-scrolling rustic rust-mode org-alert tramp-hdfs gnuplot atom-one-dark-theme lsp-haskell presentation highlight-doxygen lsp-java lsp-pyright cmake-mode cpputils-cmake cmake-ide org-pomodoro org-special-block-extras company-ctags ox-reveal org-super-agenda code-stats lsp-intellij dockerfile-mode docker ob-ipython graphviz-dot-mode highlight-indentation vterm latex-pretty-symbols pretty-symbols web-mode zzz-to-char dtrt-indent anaconda-mode company-anaconda unicode-fonts gradle-mode kotlin-mode origami ewal-doom-themes ewal impatient-mode company-lsp format-all yaml-mode ox-md hl-todo evil-multiedit treemacs-all-the-icons evil-collection use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:extend t :background "gray10"))))
+ '(org-block-begin-line ((t (:extend t :background "#1c1e1f" :foreground "#555556" :overline nil :underline t))))
+ '(org-block-end-line ((t (:inherit org-block-begin-line :extend t :overline t :underline nil))))
+ '(org-document-title ((t (:foreground "#fd971f" :weight bold :height 1.3))))
+ '(org-ellipsis ((t (:foreground "red"))))
+ '(org-latex-and-related ((t (:inherit nil :foreground "tomato" :weight bold))))
+ '(org-level-1 ((t (:inherit outline-1 :extend t :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :extend t :height 1.1))))
+ '(org-verbatim ((t (:foreground "#b6e63e" :box (:line-width (2 . 2) :color "dim gray" :style released-button))))))
