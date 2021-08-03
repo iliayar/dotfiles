3f8cdcbf5c8120255ff9efbbf5eaf12f43438ed0
Ilya <iliayar3@gmail.com>
2021-08-03 05:30:39 +0300

    Reworked Emacs config

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,6 +1,10 @@
 (setq comp-deferred-compilation t)
 (org-babel-load-file "~/.emacs.d/config.org")
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(ein:output-area-inlined-images t)
  '(package-selected-packages
     '(org-roam ox-rss nim-mode jupyter ein nlinum-relative elfeed-org elfeed-goodies elfeed lsp-ivy gcmh haskell-lsp evil-mu4e list-packages-ext mu4e smooth-scroll smooth-scrolling rustic rust-mode org-alert tramp-hdfs gnuplot atom-one-dark-theme lsp-haskell presentation highlight-doxygen lsp-java lsp-pyright cmake-mode cpputils-cmake cmake-ide org-pomodoro org-special-block-extras company-ctags ox-reveal org-super-agenda code-stats lsp-intellij dockerfile-mode docker ob-ipython graphviz-dot-mode highlight-indentation vterm latex-pretty-symbols pretty-symbols web-mode zzz-to-char dtrt-indent anaconda-mode company-anaconda unicode-fonts gradle-mode kotlin-mode origami ewal-doom-themes ewal impatient-mode company-lsp format-all yaml-mode ox-md hl-todo evil-multiedit treemacs-all-the-icons evil-collection use-package)))
@@ -9,6 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(italic ((t (:slant italic :family "Hack"))))
  '(org-block ((t (:extend t :background "gray10"))))
  '(org-block-begin-line ((t (:extend t :background "#1c1e1f" :foreground "#555556" :overline nil :underline t))))
  '(org-block-end-line ((t (:inherit org-block-begin-line :extend t :overline t :underline nil))))
