d1518c77bc4e2f49a1e90eb7bb274db3be3ace6b
iliayar <iliayar3@gmail.com>
2021-09-01 20:25:47 +0300

    Fix wallpapers

--- modules/themes/wallpapers.nix
+++ modules/themes/wallpapers.nix
@@ -5,26 +5,21 @@ pkgs.stdenv.mkDerivation rec {
   pname = "wallpapers";
   version = "1.0.0";
 
-  src = pkgs.fetchurl {
+  src = pkgs.fetchzip {
     url = "https://imgur.com/a/eptG3El/zip";
-    sha256 = "0z38zv4p5q0zhaisq6gcvavl1ny5ha4gbd7kd49d3zx0y6wjhkbi";
+    sha256 = "0ddk26bbkcwbf10y0qdrizh4bpar88hq8qsxcy52y1xv5ib3f0f0";
+    extension = "zip";
+    stripRoot = false;
   };
 
   buildInputs = with pkgs; [
     unzip
   ];
 
-  phases = [ "unpackPhase" "installPhase" ];
-
-  unpackPhase = ''
-              mkdir wall
-              unzip $src -d wall
-  '';
-
   installPhase = ''
-               mv "wall/11 - FRiMiAg.png" wall/Neofetch.png
+               mv "11 - FRiMiAg.png" Neofetch.png
                mkdir -p $out
-               mv wall/* $out/
+               mv * $out/
   '';
 
 }
