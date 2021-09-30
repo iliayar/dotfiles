7711e272346420c9ba28a418fed0cc1faaa2628f
iliayar <iliayar3@gmail.com>
2021-09-30 11:30:30 +0300

    tex: texc snippet

--- modules/dev/latex.nix
+++ modules/dev/latex.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
+let
+  texc = pkgs.writeShellScriptBin "texc" ''
+       ${pkgs.texlive.combined.scheme-full}/bin/pdflatex -interaction=nonstopmode -shell-escape
+  '';
+in
 {
   home.packages = with pkgs; [
     tectonic
+    texc
   ];
 
   programs.texlive = {
