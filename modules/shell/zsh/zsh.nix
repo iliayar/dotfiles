1a34d9fb196a1f04c4f6b8e7647def951e61d65e
Ilya Yaroshevskiy <iliayar3@gmail.com>
2026-04-06 12:19:35 +0300

    feat(starship): added

--- modules/shell/zsh/zsh.nix
+++ modules/shell/zsh/zsh.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, lib, themes, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  themes,
+  ...
+}:
 
 with lib;
 
@@ -11,36 +17,110 @@ in
       enable = mkOption {
         default = false;
       };
+      starship = {
+        enable = mkOption {
+          default = true;
+        };
+        transient = mkOption {
+          default = true;
+        };
+      };
       extra = mkOption {
         default = "";
       };
     };
   };
 
-  config = mkIf cfg.enable {
-    programs.zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-
-      oh-my-zsh = {
+  config = mkMerge [
+    (mkIf cfg.enable {
+      programs.zsh = {
         enable = true;
-        plugins = [
-          "git"
-        ];
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
 
-        custom = "${./.oh-my-zsh/themes}";
-        theme = "l";
+        # FIXME: Remove it
+        oh-my-zsh = {
+          enable = true;
+        };
+
+        shellAliases = {
+          magit = "emacsclient -t -e '(magit)'";
+        };
+
+        initContent = ''
+          ${cfg.extra}
+          PATH=$PATH:$HOME/.local/bin
+        '';
       };
+    })
+    (mkIf (cfg.enable && !cfg.starship.enable) {
+      programs.zsh = {
+        oh-my-zsh = {
+          enable = true;
+          plugins = [
+            "git"
+          ];
 
-      shellAliases = {
-        magit = "emacsclient -t -e '(magit)'";
+          custom = "${./.oh-my-zsh/themes}";
+          theme = "l";
+        };
       };
+    })
+    (mkIf (cfg.enable && cfg.starship.enable) {
+      home.packages = with pkgs; [ jj-starship ];
+      programs.starship = {
+        enable = true;
+        enableZshIntegration = true;
+        enableTransience = true;
 
-      initContent = ''
-        ${cfg.extra}
-        PATH=$PATH:$HOME/.local/bin
+        settings = {
+          add_newline = false;
+          format = "$directoryλ ";
+          right_format = "\${custom.jj}";
+
+          git_branch.disabled = true;
+          git_status.disabled = true;
+
+          directory = {
+            style = "blue";
+            truncation_length = 1;
+          };
+
+          custom.jj = {
+            when = "jj-starship detect";
+            shell = [
+              "jj-starship"
+              "--bookmarks-display-limit"
+              "1"
+            ];
+            format = "$output";
+          };
+        };
+      };
+    })
+    (mkIf (cfg.enable && cfg.starship.enable && cfg.starship.transient) {
+      programs.zsh.plugins = [
+        {
+          name = "zsh-transient-prompt";
+          file = "transient-prompt.zsh-theme";
+          src = pkgs.fetchFromGitHub {
+            owner = "olets";
+            repo = "zsh-transient-prompt";
+            rev = "bdff553570d8ef46f500daf41364bf5c06a11de5";
+            sha256 = "sha256-+Tw9TFHtBMrx3JSHmohopZHWeLKUZWzng4NFHCJxLZk=";
+          };
+        }
+      ];
+      programs.zsh.initContent = ''
+        TRANSIENT_PROMPT_PROMPT='$(starship prompt --terminal-width="$COLUMNS" --keymap="''${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="''${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="''${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+        TRANSIENT_PROMPT_RPROMPT='$(starship prompt --right --terminal-width="$COLUMNS" --keymap="''${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="''${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="''${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+        TRANSIENT_PROMPT_TRANSIENT_PROMPT='$(starship prompt --profile transient)'
       '';
-    };
-  };
+      programs.starship = {
+        settings = {
+          profiles.transient = "$directory\\\$ ";
+        };
+      };
+    })
+  ];
 }
